8ae3221dd0389c6f9918373fa0effe6f8ec1a38c
dushyantfst <dushyant_fst@yahoo.com>
2017-01-17 20:27:40 +0530

    Update unixcoordinates.sh

--- unixcoordinates.sh
+++ unixcoordinates.sh
@@ -2,8 +2,8 @@
 #please make this make file executable using : chmod u+x .....  , download jq shell using brew or any online tool
 echo "Save info about application running"
 
-#addressLineOne="$(curl -H 'access_token : Zs0nTQB-ujOSV0KmEoPhBx2E6-Ab_GKO' -H 'Content-Type : application/json' http://api.masterdatanode.com/demo/greeting/find?LANGUAGE=ENGLISH | jq -r '.greeting' | jq -r '.[].TRANSLATION')"
-# * */6 * * * /Volumes/MyMac/development/scheduler/unixprocess.sh
+#Schedule Cron Job
+# * */6 * * * /....../scheduler/unixprocess.sh
 N=2
 PROCESS=command
 number=$(ps -ax | grep  Applications | wc -l )
