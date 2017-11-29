039b5fb21a142e70985e2b005dc7ee022dc297d7
dushyantfst <dushyant_fst@yahoo.com>
2017-11-29 19:26:57 +0530

    Machine and Device Tracking App

---
+++ README.md
@@ -0,0 +1,21 @@
+# Machine and Device Tracking App
+
+Machine Tracking App, is a tracking App that tracks machines IP, its location, latitude and Longitude. Run shell script to push Ram Memory usage to ArcTop Labs #MasterDataNode Data Solution Technology.
+once the data is been pushed to cloud, open locationmap.html which open up Google Map and All you Machine location is available with maker.
+Google Map is been used to create chart applciation, if one face error replace google Map API key.
+bash script uses FreegioIP.net to get current location, json data is been parsed using jq library.
+This is an good way to start your tracking of connected devices, which give clear information. This can be enhanced to track anything that connect to internet.
+ 
+## Storage Service ##
+ArcTop Labs : MasterDataNode Data Solution
+<p>Login to <a href="https://www.masterdatanode.com"> www.masterdatanode.com </a> to get : Access_Token , its Free</p>
+<p>Create an App after login, copy and paste App Name (track) against ##APPNAME## string on .sh file</p>
+<p>Copy and Replace the access_token (available in your login) against ####ACCESSTOKEN###### string on .sh file</p>
+<p>Copy and Replace the access_token against ####ACCESSTOKEN###### string on locationmap.html file</p>
+<p>Copy and Replace the access_token against ######GoogleMapAPI########### string on locationmap.html file</p>
+            
+
+![alt text](https://github.com/ArcTopLabs/MemoryDashboard/blob/master/screenshot/Ram%20Memory%20dashboard.png)
+![alt text](https://github.com/ArcTopLabs/MemoryDashboard/blob/master/screenshot/Screen%20Shot%202017-11-29%20at%205.50.19%20PM.png)
+
+
--- unixcoordinates.sh
+++ unixcoordinates.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 #please make this make file executable using : chmod u+x .....  , download jq shell using brew or any online tool
-echo "Save info about application running"
+echo "Save info about current location using freegeoip.net"
 
-#addressLineOne="$(curl -H 'access_token : Zs0nTQB-ujOSV0KmEoPhBx2E6-Ab_GKO' -H 'Content-Type : application/json' http://api.masterdatanode.com/demo/greeting/find?LANGUAGE=ENGLISH | jq -r '.greeting' | jq -r '.[].TRANSLATION')"
-# * */6 * * * /Volumes/MyMac/development/scheduler/unixprocess.sh
-N=2
-PROCESS=command
-number=$(ps -ax | grep  Applications | wc -l )
-echo $number
-PROCESS=command
-coordinate=$(curl -s http://whatismycountry.com/ | (sed -n 's/.*Coordinates \(.*\)<.*/\1/p'))
-#latitude=$(curl -s http://whatismycountry.com/ | (sed -n 's/.*Coordinates \(.*\)<.*/\1/p') | cut -d " " -f 1)
-#longitude=$(curl -s http://whatismycountry.com/ | (sed -n 's/.*Coordinates \(.*\)<.*/\1/p') | cut -d " " -f 2)
-#arr=($STRING)
-#latitude=$($coordinate | cut -d " " -f 1)
-#longitude=$($coordinate | cut -d " " -f 2)
-#echo $latitude 
-#echo $longitude    
-#echo $coordinate 
+#Freegeoip return
+#{ "ip": "122.177.114.65", "country_code": "IN", "country_name": "India", "region_code": "UP", "region_name": "Uttar Pradesh", "city": "Noida", "zip_code": "201301", "time_zone": "Asia/Kolkata", "latitude": 28.57, "longitude": 77.32, "metro_code": 0 }
+# use jq to add Machine name in returned json
 
-IFS=' ' read -r -a arra <<< "$coordinate"
-#arra=$($coordinate)
-latitude=${arra[0]}
-longitude=${arra[1]}
+coordinate=$(curl -s --get http://freegeoip.net/json/ | jq '. |= . + {"Machine" : "Machine1"}')
+echo $coordinate 
+
 
 echo coordinate are as follow latidude $latitude longitude $longitude;
-curl -H "access_token: ####Access_Token########" -H "Content-Type: application/json" -H "user-agent : chrome/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko)" --data "{ \"data\" : [{ \"latitude\" : \"$latitude\", \"longitude\" : \"$longitude\", \"machine\" : \"Mac OS\" }]}" -v -k https://api.masterdatanode.com/unix/track/save/
+curl -H "access_token:####ACCESSTOKEN######" -H "Content-Type: application/json" -H "user-agent : chrome/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko)" --data "{ \"data\" : [$coordinate ]}" https://api.masterdatanode.com/##APPNAME##/track/save/
 
